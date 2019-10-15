814b2b1152b5fb767d9921c83d822f8a1f36cbae
Tristanas <43069390+Tristanas@users.noreply.github.com>
2019-10-15 16:15:45 +0300

    Removed from Currency value in euro when buying
    I intend to set the buying price of currency in methods and not by fields.

---
+++ first-part/KreditoKortos/KreditoKortos/Currency.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KreditoKortos
+{
+    class Currency
+    {
+        public string Name;
+        public float ValueInEuro;
+    }
+}
